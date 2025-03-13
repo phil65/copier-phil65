6eaf3e6be6e75033392d3a3818f33531c6d41963
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-03-13 20:20:29 +0100

    chore: typing

--- extensions.py
+++ extensions.py
@@ -4,7 +4,9 @@ import re
 import subprocess
 import unicodedata
 from datetime import date
+from typing import Any
 
+from jinja2.environment import Environment
 from jinja2.ext import Extension
 
 
@@ -16,14 +18,18 @@ def git_user_email(default: str) -> str:
     return subprocess.getoutput("git config user.email").strip() or default
 
 
-def slugify(value, separator="-"):
-    value = unicodedata.normalize("NFKD", str(value)).encode("ascii", "ignore").decode("ascii")
+def slugify(value: Any, separator: str = "-"):
+    value = (
+        unicodedata.normalize("NFKD", str(value))
+        .encode("ascii", "ignore")
+        .decode("ascii")
+    )
     value = re.sub(r"[^\w\s-]", "", value.lower())
     return re.sub(r"[-_\s]+", separator, value).strip("-_")
 
 
 class MyExtensions(Extension):
-    def __init__(self, environment):
+    def __init__(self, environment: Environment):
         super().__init__(environment)
         environment.filters["git_user_name"] = git_user_name
         environment.filters["git_user_email"] = git_user_email
